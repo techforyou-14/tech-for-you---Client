21e1cacc2cae901a23b29b6a347dc97a4d044c50
Bryan Lojan <bryanlc9808@gmail.com>
2023-06-20 22:51:07 +0200

    add logo and create chart for water

--- src/components/home/WaterUse.jsx
+++ src/components/home/WaterUse.jsx
@@ -1,9 +1,47 @@
+import { useState, useEffect } from "react";
+import BarChart from "../ui/commons/BarChart";
 import Card from "../ui/commons/Card";
+import water from '../../constants/water.json';
+import LineChart from "../ui/commons/LineChart";
 
-export default function WaterUse(){
-return(
-    <Card>
-        <span>Water use</span>
-    </Card>
-)
+export default function WaterUse() {
+
+    const [dataWater, setDataWater] = useState({
+        labels: water.map((data) => data.year),
+        datasets: [
+            {
+                label: "Agua regenerada (m^3)",
+                data: water.map((data) => data.total),
+                backgroundColor: "#E08DAC",
+                drawBorder: false,
+            }
+        ]
+    })
+
+    useEffect(() => {
+        setDataWater({
+            labels: water.map((data) => data.year),
+            datasets: [
+                {
+                    label: "Agua regenerada (m³)",
+                    data: water.map((data) => data.total),
+                    backgroundColor: "#E08DAC"
+                }
+            ]
+
+        })
+    }, [])
+    return (
+        <Card className="gap-y-4">
+            <div>
+                <h1 className="font-bold">Agua regenerada</h1>
+                <p>
+                    La reutilización consiste en dar nuevos usos al agua una vez esta se ha saneado en las depuradoras o estaciones de regeneración de agua (ERA).
+                </p>
+            </div>
+            <div>
+                <LineChart chartData={dataWater} />
+            </div>
+        </Card>
+    )
 }
--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -6,13 +6,9 @@ function Login() {
     return (
         <div className='w-full px-[5%] md:px-[10%] grid md:grid-cols-2 py-[10vh] min-h-screen '>
             <FormLogin className='rounded-l justify-center lg:px-16' />
-            <div className=" bg-base-200 rounded-r shadow-lg hidden relative md:flex md:flex-col">
-                <div className="h-1/2 relative flex justify-center overflow-hidden ">
-                    <div className="absolute w-40 aspect-square rounded-full bg-primary bottom-0 translate-y-1/2 bg-gradient-to-r from-primary via-secondary to-primary animate-pulse "></div>
-                </div>
-                <div className="h-1/2 blur-md w-full relative flex justify-center overflow-hidden">
-                    <div className="absolute w-40 aspect-square rounded-full bg-primary top-0 -translate-y-1/2 bg-gradient-to-r from-primary via-secondary to-primary animate-pulse "></div>
-                </div>
+            <div className=" bg-base-200 rounded-r shadow-lg hidden justify-center relative md:flex md:flex-col">
+            <img src="/public/BCN4Water_transpa.png" alt="logo" className="w-full" />
+                
             </div>
         </div>
     )
